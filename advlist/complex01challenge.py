b800b09d485aba762035655df29be995af3ea879
James Vinciguerra <James.Vinciguerra@outlook.com>
2020-07-27 18:39:54 -0400

    practice challenges

---
+++ advlist/complex01challenge.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+
+animals = ['cat', 'hen', 'dog', 'pig', 'bug', 'cow']
+
+for every_element in animals:
+    print(every_element)
---
+++ challengelab02.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+col_list = ['blue', 'Columbus']
+col_list.append(1492)
+x = input('What is your name, Sailor? ')
+print(f'In {col_list[2]}, {col_list[1]} sailed the ocean {col_list[0]}. {x} fell off the boat')
