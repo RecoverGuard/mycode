4903d600f14e967253d6fc87ffe3a1d4276923ba
James Vinciguerra <James.Vinciguerra@outlook.com>
2020-07-31 11:05:02 -0400

    revised calculator

--- calculator_challenge.py
+++ calculator_challenge.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 #this challenge is to create a calculator
 
 
@@ -18,8 +19,8 @@ cont = 'yes'
 
 while cont == 'yes' or cont == 'Yes':
     print("Welcome to 'MY CALCULATOR LITE'!")
-    x = int(input("please select your first integer: "))
-    y = int(input("Please select a second integer: "))
+    x = float(input("please select your first integer: "))
+    y = float(input("Please select a second integer: "))
     operator = input("please select an operator(+, -, /, *): ")
 
 
